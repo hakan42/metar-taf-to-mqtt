576c289e09146b1adaec5b6879900017a944c1e7
Hakan Tandogan <hakan@tandogan.com>
2023-07-30 20:31:13 +0200

    Run each script in its dedicated directory
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- fetch-data-metar2xml.sh
+++ fetch-data-metar2xml.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 STATIONS=""
 
@@ -44,7 +44,7 @@ STATIONS="${STATIONS} "
 STATIONS="${STATIONS} "
 STATIONS="${STATIONS} "
 
-TARGET=./target/aviationweather
+TARGET=./target/metar2xml
 
 rm -rf ./${TARGET}
 mkdir -p ./${TARGET}
@@ -52,25 +52,27 @@ mkdir -p ./${TARGET}
 for s in ${STATIONS}
 do
     curl \
-	--silent \
-	--output ./${TARGET}/${s}-FULL.xml \
-	"https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=5&mostRecentForEachStation=true&stationString="${s}
+        --silent \
+        --output ./${TARGET}/${s}-FULL.xml \
+	"https://metaf2xml.sourceforge.io/cgi-bin/metaf.pl?lang=en&format=xml&mode=latest&hours=24&unit_temp=C&type_metaf=icao&msg_metaf="${s}"&type_synop=synop&msg_synop=&type_buoy=buoy&msg_buoy=&type_amdar=amdar&msg_amdar="
 
     xmllint \
-	--xpath '//flight_category/text()' ${TARGET}/${s}-FULL.xml \
-	> ${TARGET}/${s}-category
+	--xpath '(//info)[1]' ${TARGET}/${s}-FULL.xml \
+	> ${TARGET}/${s}-info.xml
 
-    echo ${s}
-    cat ${TARGET}/${s}-category
-
+    xmllint --xpath '//info/text()' ${TARGET}/${s}-info.xml \
+	> ${TARGET}/${s}-name.txt
+    
     #
     # Now publish it via MQTT
     #
+    # TODO would be nice to a check and publish-if-not-changed
+    #
 
     mosquitto_pub \
-	--retain \
-	--topic metarmap/${s}/flight_category \
-	--file ${TARGET}/${s}-category
+     	--retain \
+	--topic metarmap/${s}/name \
+	--file ${TARGET}/${s}-name.txt
 
 done
 
