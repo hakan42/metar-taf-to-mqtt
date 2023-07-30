0a3da987583e20e7e25284b146b26ae78e752bc1
Hakan Tandogan <hakan@tandogan.com>
2023-07-30 17:15:37 +0200

    For the time being, use a simple shell script with curl, xmllint and mosquitto_pub
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

---
+++ fetch-data.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+STATIONS=""
+
+STATIONS="${STATIONS} EDDM" # Munich
+STATIONS="${STATIONS} EDDN" # Nuremberg
+STATIONS="${STATIONS} EDJA" # Memmingen
+STATIONS="${STATIONS} EDMA" # Augsburg
+STATIONS="${STATIONS} EDMO" # Oberpfaffenhofen
+
+STATIONS="${STATIONS} ETEB" # Ansbach
+STATIONS="${STATIONS} ETHA" # Altenstadt
+STATIONS="${STATIONS} ETHL" # Laupheim
+STATIONS="${STATIONS} ETIH" # Hohenfels
+STATIONS="${STATIONS} ETSI" # Ingolstadt Manching
+STATIONS="${STATIONS} ETSL" # Lechfeld
+STATIONS="${STATIONS} ETSN" # Neuburg
+
+STATIONS="${STATIONS} LOWI" # Insbruck
+
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+STATIONS="${STATIONS} "
+
+TARGET=./target
+
+rm -rf ./${TARGET}
+mkdir -p ./${TARGET}
+
+for s in ${STATIONS}
+do
+    curl \
+	--silent \
+	--output ./${TARGET}/${s}-FULL.xml \
+	"https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=5&mostRecentForEachStation=true&stationString="${s}
+
+    xmllint \
+	--xpath '//flight_category/text()' ${TARGET}/${s}-FULL.xml \
+	> ${TARGET}/${s}-category
+
+    echo ${s}
+    cat ${TARGET}/${s}-category
+
+    #
+    # Now publish it via MQTT
+    #
+
+    mosquitto_pub \
+	--retain \
+	--topic metarmap/${s}/flight_category \
+	--file ${TARGET}/${s}-category
+
+done
+
+ls -l ${TARGET}
