90f5e028db01208a17446d6a285e871db3863ce6
Hakan Tandogan <hakan@tandogan.com>
2024-08-01 13:40:58 +0200

    Use absolute paths for cron usage
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- install-metaf2xml.sh
+++ install-metaf2xml.sh
@@ -1,17 +1,19 @@
 #!/bin/sh -x
 
-HERE=$(pwd -P)
+HERE=$(dirname $(realpath $0))
 
 TARGET=${HERE}/target
 
 rm -rf ${TARGET}
 
-git -c http.sslVerify=false clone https://git.code.sf.net/p/metaf2xml/code ${TARGET}/metaf2xml-code
+# git -c http.sslVerify=false clone https://git.code.sf.net/p/metaf2xml/code ${TARGET}/metaf2xml-code
+git clone https://git.code.sf.net/p/metaf2xml/code ${TARGET}/metaf2xml-code
+
 cd ${TARGET}/metaf2xml-code
 git checkout r2.8
 git checkout unstable
 METAF2XML=${HERE}/metaf2xml ./install.pl all
 
-cd METAF2XML=${HERE}/metaf2xml
+cd ${TARGET}/metaf2xml-code
 ./metaf.pl --help     2> ${TARGET}/metaf.help
 ./metaf2xml.pl --help 2> ${TARGET}/metaf2xml.help
